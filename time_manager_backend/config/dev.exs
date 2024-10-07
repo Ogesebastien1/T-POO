9505459681ecfccc43d7576a5adf3e675e354f2b
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-10-07 17:17:50 +0200

    Refactor docker-compose.yml and entrypoint.sh
    - Refactor docker-compose.yml to improve service dependencies and health checks.
    - Remove entrypoint.sh script as it is no longer needed.

--- time_manager_backend/config/dev.exs
+++ time_manager_backend/config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :time_manager_backend, TimeManagerBackend.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "time_manager_backend_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
