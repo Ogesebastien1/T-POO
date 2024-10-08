4d90805ac0b15311ea7583843295cdd47f4df8cf
AlxisHenry <alexis.henry150357@gmail.com>
2024-10-08 11:01:29 +0200

    fix: clean docker setup

--- time_manager_backend/entrypoint.sh
+++ time_manager_backend/entrypoint.sh
@@ -1,28 +1,7 @@
 #!/bin/sh
 
-DB_SERVICE_NAME=db
-DB_USER=postgres
-DB_PASSWORD=postgres
-DB_NAME=postgres
-
-echo "Waiting for database service: $DB_SERVICE_NAME"
-until PGPASSWORD=$DB_PASSWORD pg_isready -h $DB_SERVICE_NAME -U $DB_USER -d $DB_NAME; do
-  echo "Waiting for database to be ready..."
-  sleep 2
-done
-
-echo "Database is ready"
-
-echo "Listing files in /app:"
-ls -la /app
-
 cd time_manager_backend
-
 mix deps.get
-
-# Créer et migrer la base de données
 mix ecto.create
 mix ecto.migrate
-
-# Démarrer le serveur Phoenix
 mix phx.server
