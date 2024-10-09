1a4236cbc4430bff9581ecb19ff921da26b8c9e5
El Hilali Mouad <elhilali.mad@gmail.com>
2024-10-09 11:28:57 +0200

    feat: working time service and post controller

--- TimeManager/lib/time_manager/timetracking/infrastructure/controllers/working-time.controller.ex
+++ TimeManager/lib/time_manager/timetracking/infrastructure/controllers/working-time.controller.ex
@@ -1,9 +1,38 @@
-defmodule TimeManager.TimeTracking.Infrastructure.WorkingTimeController do
+defmodule TimeManagerWeb.TimeTracking.Infrastructure.WorkingTimeController do
   use TimeManagerWeb, :controller
+  alias TimeManager.TimeTracking.{WorkingTime , Application.ManageWorkingTimeService}
+  alias TimeManager.TimeTracking.Infrastructure.{WorkingTimePresenter}
 
   action_fallback TimeManagerWeb.FallbackController
 
-  
+  # def index(conn, %{"userID" => userID, "id" => id}) do
+
+
+  # end
+
+  # def index(conn, %{"userID" => userID, "start" => start, "end" => :end}) do
+
+  # end
+
+  # def index(conn, _params) do
+  #   conn
+  #   |> put_status(:bad_request)
+  #   |> put_view(TimeManagerWeb.ErrorView)
+  #   |> render("400.json", message: "Invalid parameters")
+  # end
+
+  # def create(conn, %{"userID" => userID, "working_time" => working_time_params}) do
+  #   with {:ok, %WorkingTime{} = working_time} <- ManageWorkingTimeService
+  # end
+
+  def create(conn , %{"userID" => userID, "working_time" => working_time_params}) do
+    with {:ok, %WorkingTime{} = working_time} <- ManageWorkingTimeService.create_working_time(%{"userID" => userID, "working_time" => working_time_params}) do
+      conn
+      |> put_status(:created)
+      |> put_view(WorkingTimePresenter)
+      |> render(:present_working_time, working_time: working_time)
+    end
+  end
 
 
 
