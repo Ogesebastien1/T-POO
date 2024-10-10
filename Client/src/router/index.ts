21897750c861153e8936e9db9f34d73fa0a1905b
AlxisHenry <alexis.henry150357@gmail.com>
2024-10-10 12:00:50 +0200

    feat: init dashboard

--- Client/src/router/index.ts
+++ Client/src/router/index.ts
@@ -1,14 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'dashboard',
+      component: () => import('../views/DashboardView.vue')
     },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: () => import('../views/StatsView.vue')
+    }
   ]
 })
 
