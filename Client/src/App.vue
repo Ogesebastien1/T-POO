d51ce8d51ab2319ba92004f550b250b490343770
AlxisHenry <alexis.henry150357@gmail.com>
2024-10-10 14:42:06 +0200

    feat: show router viex

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
-  import { Sidebar } from '@/components'
+import { Sidebar } from '@/components'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <Sidebar />
+  <div class="flex min-h-screen w-full flex-col bg-muted/40">
+    <Sidebar />
+
+    <main class="flex flex-col sm:gap-4 sm:py-4 sm:pl-16 pr-4">
+      <RouterView />
+    </main>
+  </div>
 </template>
+
--- Client/src/views/DashboardView.vue
+++ Client/src/views/DashboardView.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 </script>
 
 <template>
+    <div>
+        <Card>
+        <CardHeader>
+            <CardTitle>Dashboard</CardTitle>
+        </CardHeader>
+        <CardContent>
+            <CardDescription>
+            Welcome to the dashboard
+            </CardDescription>
+        </CardContent>
+        <CardFooter>
+            <router-link to="/about">About</router-link>
+        </CardFooter>
+        </Card>
+    </div>
 </template>
