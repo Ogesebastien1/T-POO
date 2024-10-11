ba4141e8402a7c63f9810c639ec387d39dc1a20a
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-10-11 11:19:02 +0200

    feat: Add AreaChart component to StatsView
    This commit adds the AreaChart component to the StatsView, allowing for the display of statistical data in a visually appealing way.

---
+++ Client/src/components/AreaChart.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { AreaChart } from '@/components/ui/chart-area'
+
+const data = [
+  {
+    name: 'Jan',
+    value: 100
+  },
+  {
+    name: 'Feb',
+    value: 200
+  },
+  {
+    name: 'Mar',
+    value: 150
+  },
+  {
+    name: 'Apr',
+    value: 300
+  }
+]
+</script>
+
+<template>
+    <div class="mx-auto grid w-full gap-2">
+      <h1 class="text-2xl font-semibold mb-3 mt-5 ml-5">Statistics</h1>
+      <AreaChart
+        :data="data"
+        :colors="['blue', 'pink', 'orange', 'red']"
+        :categories="['name', 'value']"
+        index="name"
+      />
+    </div>
+  </template>
--- Client/src/components/index.ts
+++ Client/src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Sidebar } from './Sidebar.vue'
+export { default as AreaChart } from './AreaChart.vue'
