4c39ac8747af40a5b4808f701621dc4a120b2b28
AlxisHenry <alexis.henry150357@gmail.com>
2024-10-14 11:35:29 +0200

    fix: cors error

--- Server/lib/time_manager/api-config/endpoint.ex
+++ Server/lib/time_manager/api-config/endpoint.ex
@@ -46,7 +46,7 @@ defmodule TimeManagerWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
+  plug CORSPlug, origin: ["*"], methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
   plug Plug.Session, @session_options
   plug TimeManagerWeb.Router
-  plug CORSPlug, origin: "*", methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 end
