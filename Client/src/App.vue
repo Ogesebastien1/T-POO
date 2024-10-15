b9c86027d3f4b568c24160b342ecd4909fec9b37
AlxisHenry <alexis.henry150357@gmail.com>
2024-10-15 16:54:21 +0200

    fix: v-if instead of v-show for RouterView

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -8,11 +8,11 @@ const authStore = useAuthStore()
 </script>
 
 <template>
-  <main class="w-full lg:grid lg:min-h-[600px] h-screen" v-show="!authStore.isLoggedIn">
+  <main class="w-full lg:grid lg:min-h-[600px] h-screen" v-if="!authStore.isLoggedIn">
     <RouterView />
   </main>
 
-  <div class="flex min-h-screen w-full flex-col bg-muted/40" v-show="authStore.isLoggedIn">
+  <div class="flex min-h-screen w-full flex-col bg-muted/40" v-if="authStore.isLoggedIn">
     <Sidebar />
 
     <main class="flex flex-col sm:mt-0 mt-6 sm:gap-4 sm:py-4 ml-6 sm:ml-20 mr-6 mb-6">
