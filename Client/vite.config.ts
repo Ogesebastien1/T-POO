fb831fb1e5290c03183307febed8ff2cac9eacb2
AlxisHenry <alexis.henry150357@gmail.com>
2024-10-21 20:26:08 +0200

    feat: pwa

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
@@ -12,17 +13,59 @@ import autoprefixer from 'autoprefixer'
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
+      plugins: [tailwind(), autoprefixer()]
+    }
   },
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: [
+        "**/*",
+      ],
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Vite App',
+        short_name: 'Vite App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/pwa/icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        "screenshots": [
+          {
+            "src": "/pwa/screenshots/1.jpg",
+            "sizes": "1080x1920",
+            "type": "image/png",
+            "form_factor":"wide"
+          },
+          {
+            "src": "/pwa/screenshots/2.jpg",
+            "sizes": "1080x1920",
+            "type": "image/png"
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
