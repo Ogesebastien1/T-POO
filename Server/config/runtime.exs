fe0ff9ce4ec629141e8e57e92c5c7b2292e0a6c4
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-10-24 13:36:37 +0200

    Refactor Dockerfile.prod and entrypoint.sh.prod for Server and Client builds

--- Server/config/runtime.exs
+++ Server/config/runtime.exs
@@ -54,7 +54,7 @@ if config_env() == :prod do
   config :time_manager, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :time_manager, TimeManagerWeb.Endpoint,
-    url: [host: host, port: 80],
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
