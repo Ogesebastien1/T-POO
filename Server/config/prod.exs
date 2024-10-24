ddf8e16420584e1abef46ae620a682e3c53bf194
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-10-24 13:31:03 +0200

    Refactor Dockerfile.prod and entrypoint.sh.prod for Server and Client builds

--- Server/config/prod.exs
+++ Server/config/prod.exs
@@ -1,15 +1,5 @@
 import Config
 
-# Configure your database
-config :time_manager, TimeManager.Repo,
-  username: System.get_env("PGUSER"),
-  password: System.get_env("PGPASSWORD"),
-  hostname: System.get_env("PGHOST"),
-  database: System.get_env("PGDATABASE"),
-  stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
-
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: TimeManager.Finch
 
