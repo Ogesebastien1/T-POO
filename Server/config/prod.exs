6f28cb3077bb182a685808cb47e350c0cfc4bd07
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-10-24 11:40:02 +0200

    Refactor Dockerfile.prod and entrypoint.sh.prod for Server and Client builds

--- Server/config/prod.exs
+++ Server/config/prod.exs
@@ -1,5 +1,25 @@
 import Config
 
+# Configure your database
+config :time_manager, TimeManager.Repo,
+  username: System.get_env("PGUSER"),
+  password: System.get_env("PGPASSWORD"),
+  hostname: System.get_env("PGHOST"),
+  database: System.get_env("PGDATABASE"),
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
+config :time_manager, TimeManagerWeb.Endpoint,
+  # Binding to loopback ipv4 address prevents access from other machines.
+  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
+  http: [ip: {0, 0, 0, 0}, port: 4000],
+  check_origin: false,
+  code_reloader: true,
+  debug_errors: true,
+  secret_key_base: "Gr/kHCXYkk6JQFfpzKCoyE7y5HyK9/urd4TMwGqDpqzRueapgvU6IC9KZfN7W5yd",
+  watchers: []
+
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: TimeManager.Finch
 
